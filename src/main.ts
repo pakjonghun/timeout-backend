ea9eaf8b639014d9fa083b82b1a9e2664da27bab
정훈박 <fireking5997@gmail.com>
2022-06-23 12:19:58 +0900

    fix:cors

--- src/main.ts
+++ src/main.ts
@@ -9,11 +9,7 @@ async function bootstrap() {
   app.setGlobalPrefix('api');
   app.use(cookieParser());
   app.enableCors({
-    origin: [
-      'http://localhost',
-      'http://localhost:8080',
-      'http://localhost:3000',
-    ],
+    origin: [process.env.URL],
     credentials: true,
   });
   app.useGlobalPipes(
