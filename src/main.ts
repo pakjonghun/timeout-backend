5181764de1f18073cd7265d0fd585cf3771aa2e0
정훈박 <fireking5997@gmail.com>
2022-07-05 03:50:43 +0900

    fix:origin 추가

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import * as cookieParser from 'cookie-parser';
 async function bootstrap() {
   const allowList = [
     'http://fireking5997.com',
+    'http://www.fireking5997.com',
     'http://localhost:3000',
     process.env.BASE_URL,
   ];
